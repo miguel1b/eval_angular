46f88500096181706b072341450b9dc8324b849e
hbhuby <hbhuby@synopsis.ws>
2018-12-03 20:44:32 -0500

    Avance crud angular

---
+++ CrudDosDias/src/main/java/com/crud/CrudDosDias/CrudDosDiasApplication.java
@@ -0,0 +1,12 @@
+package com.crud.CrudDosDias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDosDiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDosDiasApplication.class, args);
+	}
+}
